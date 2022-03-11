39d0e0d36f15f6743a1093e1be7fa3dab3417517
begalievn <begaliev.info@gmail.com>
2022-03-11 10:38:03 +0600

    localStorage added

--- script.js
+++ script.js
@@ -1,109 +1,52 @@
-let addTask = document.getElementById("add-task");
-const inputTask = document.getElementsByClassName("input-task")[0];
-const submitButton = document.getElementById("submit-button");
-const inputValue = document.getElementById("taskName");
-const taskContainer = document.getElementsByClassName("tasks-container")[0];
+// Array of tasks
+const arrayOfTasks = JSON.parse(localStorage.getItem("tasks"));
+
+// Container for FORM
+const containerOfForm = document.getElementsByClassName("input-task")[0];
+
+// Form element
+const formElement = document.getElementsByClassName("form-adding-tasks")[0];
+
+/* Plus Button to show Input field */
+const plusButton = document.getElementById("plus-button");
+// PlusButton event listener
+plusButton.addEventListener("click", function () {
+  containerOfForm.style.display = "block";
+  this.style.visibility = "hidden";
+});
+
+/* ADDING A TASK */
+// Input field
+const inputField = document.getElementById("taskName");
+
+/* ADD button  */
+const addButton = document.getElementById("submit-button");
+// add button event listener
+addButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  let task = {
+    text: inputField.value,
+    status: false,
+  };
+  if (inputField.value != "") {
+    arrayOfTasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(arrayOfTasks));
+  }
+
+  formElement.reset(); // reset an input field
+
+  plusButton.style.visibility = "visible";
+  containerOfForm.style.display = "none";
+
+  console.log(JSON.parse(localStorage.getItem("tasks")));
+});
+
+/* CANCEL button */
 const cancelButton = document.getElementById("cancel-button");
-
-addTask.addEventListener("click", () => {
-  console.log("button is clicked");
-  inputTask.style.display = "block";
-  addTask.style.visibility = "hidden";
+// cancel button event listener
+cancelButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem("tasks"))); // to check if localStorage is storing tasks
+  containerOfForm.style.display = "none";
+  plusButton.style.visibility = "visible";
 });
-
-submitButton.addEventListener("click", () => {
-  let contentOfInput = inputValue.value;
-  console.log(contentOfInput);
-  const newTask = document.createElement("div");
-  newTask.setAttribute("draggable", true);
-  newTask.className += "task";
-  const newPara = document.createElement("p");
-  let textnode = document.createTextNode(contentOfInput);
-  newPara.appendChild(textnode);
-  const leftBorder = document.createElement("div");
-  leftBorder.className += "task-left-border";
-  const taskMark = document.createElement("div");
-  taskMark.className += "task-mark";
-  taskMark.addEventListener("click", () => {
-    if (taskMark.classList.contains("task-mark-active")) {
-      alert("Drag the task to delete it!");
-    }
-    taskMark.classList += " task-mark-active";
-
-    console.log("mark is clicked");
-  });
-  newTask.appendChild(leftBorder);
-  newTask.appendChild(newPara);
-  newTask.appendChild(taskMark);
-  if (contentOfInput != "") {
-    taskContainer.appendChild(newTask);
-  }
-  newTask.setAttribute("id", generateRandomId());
-  newTask.setAttribute("ondragend", "deleteTask(event, this.id)");
-
-  addTask.style.visibility = "visible";
-  inputTask.style.display = "none";
-  inputValue.value = "";
-});
-
-inputValue.addEventListener("keypress", (event) => {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    console.log("enter is clicked");
-    let contentOfInput = inputValue.value;
-    console.log(contentOfInput);
-    const newTask = document.createElement("div");
-    newTask.setAttribute("draggable", true);
-    newTask.className += "task";
-    const newPara = document.createElement("p");
-    let textnode = document.createTextNode(contentOfInput);
-    newPara.appendChild(textnode);
-    const leftBorder = document.createElement("div");
-    leftBorder.className += "task-left-border";
-    const taskMark = document.createElement("div");
-    taskMark.className += "task-mark";
-    taskMark.addEventListener("click", () => {
-      if (taskMark.classList.contains("task-mark-active")) {
-        alert("Drag the task to delete it!");
-      }
-      taskMark.classList += " task-mark-active";
-
-      console.log("mark is clicked");
-    });
-
-    newTask.appendChild(leftBorder);
-    newTask.appendChild(newPara);
-    newTask.appendChild(taskMark);
-    if (contentOfInput != "") {
-      taskContainer.appendChild(newTask);
-    }
-    newTask.setAttribute("id", generateRandomId());
-    newTask.setAttribute("ondragend", "deleteTask(event, this.id)");
-
-    addTask.style.visibility = "visible";
-    inputTask.style.display = "none";
-    inputValue.value = "";
-  }
-});
-
-cancelButton.addEventListener("click", () => {
-  inputValue.value = "";
-  addTask.style.visibility = "visible";
-  inputTask.style.display = "none";
-});
-
-const deleteTask = function (event, id) {
-  event.preventDefault();
-  document.getElementById(id).remove();
-};
-
-const output = function () {
-  console.log("drag is over");
-};
-
-const generateRandomId = function () {
-  let id = "task-";
-  id += Date.now();
-  console.log(id);
-  return id;
-};
